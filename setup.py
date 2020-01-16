faa7b5133e6745aaaf431ef489ae0c685c22d889
micahmelling <micahmelling@gmail.com>
2020-01-15 22:49:53 -0600

    trying to get this to work

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/micahmelling",
-    packages=setuptools.find_packages(where='/ds_helpers'),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
